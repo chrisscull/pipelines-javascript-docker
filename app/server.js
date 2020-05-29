b9eb016715330a831b607434a7fa8a68fd953b78
chrisscull <66028817+chrisscull@users.noreply.github.com>
2020-05-28 18:47:30 -0700

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world.... Also Jeremy has a funny looking face, but dont tell him I said so\n');
 });
 
 app.listen(PORT, HOST);
